ddcca541d9fbaea38351fd6525ff131e4832a623
Tony Robalik <trobalik@squareup.com>
2025-06-09 12:15:10 -0700

    Stop using deprecated function.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -29,7 +29,7 @@ publishing {
     repositories {
         maven {
             name = "build"
-            url  = uri("${buildDir}/repo")
+            url = uri(layout.buildDirectory.dir("repo"))
         }
     }
 }
